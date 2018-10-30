ac57b714ffa1e6b5e86cc271441471ca8fb9f923
hhchaos <HHChaos@outlook.com>
2018-10-30 11:42:01 +0800

    BindableBase adds a new method update property

--- HHChaosToolkit.UWP/Mvvm/BindableBase.cs
+++ HHChaosToolkit.UWP/Mvvm/BindableBase.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.ApplicationModel;
 
 namespace HHChaosToolkit.UWP.Mvvm
 {
     public abstract class BindableBase : INotifyPropertyChanged
     {
+        [IgnoreDataMember] public bool IsInDesignMode => DesignMode.DesignModeEnabled;
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        [IgnoreDataMember]
-        public bool IsInDesignMode => DesignMode.DesignModeEnabled;
-
-        public void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
             if (DesignMode.DesignModeEnabled)
                 return;
@@ -25,13 +21,36 @@ namespace HHChaosToolkit.UWP.Mvvm
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public bool Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        public void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propName = GetPropertyName(propertyExpression);
+            if (!string.IsNullOrEmpty(propName))
+                RaisePropertyChanged(propName);
+        }
+
+        protected static string GetPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression.Body is MemberExpression memberExpress)
+                return memberExpress.Member.Name;
+
+            return string.Empty;
+        }
+
+        protected bool Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(storage, value))
                 return false;
             storage = value;
             RaisePropertyChanged(propertyName);
             return true;
+        }
+
+        protected bool Set<T>(Expression<Func<T>> propertyExpression, ref T storage, T value)
+        {
+            var propName = GetPropertyName(propertyExpression);
+            if (!string.IsNullOrEmpty(propName))
+                return Set(ref storage, value, propName);
+            return false;
         }
     }
 }
